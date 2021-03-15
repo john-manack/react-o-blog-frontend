bf26ac2ecdb0476fed082721c2013ab389f610f1
John Manack <jmnck28@gmail.com>
2021-03-15 14:25:39 -0400

    functioning app

--- src/components/AlbumDetails.jsx
+++ src/components/AlbumDetails.jsx
@@ -1,9 +1,34 @@
-import { useEffects, useState } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const AlbumDetails = () => {
+    const { album_slug } = useParams({});
+    const [album, setAlbum] = useState();
+    console.log("slug is",album_slug)
+
+    useEffect(() => {
+        (async () => {
+            const albumInfo = await fetch (`http://127.0.0.1:3000/entries/${album_slug}`).then(response => response.json());
+            setAlbum(albumInfo);
+        })();
+    },[setAlbum, album_slug]);
+
     return (
         <>
-            <p>Details Here</p>
+            {!!album ? 
+                (
+                    <>
+                        <h1>{album.album_name}</h1>
+                        <h2>By {album.band_name}</h2>
+                        <img src={`${album.cover}`} alt={`${album.album_name} album cover`}/>
+                        <hr/>
+                        <h3>Comments</h3>
+                        <p>{album.comment_message} - {album.stars} Stars</p>
+                    </>
+                ) : (
+                    <p>Getting Album Info</p>
+                )
+            }
         </>
     )
 }
--- src/components/Albums.jsx
+++ src/components/Albums.jsx
@@ -1,21 +1,42 @@
-import { useEffects, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Link, useHistory } from 'react-router-dom';
 import AlbumDetails from './AlbumDetails';
 
 const Albums = () => {
+    const [albums, setAlbums] = useState([]);
+    const history = useHistory();
     
-    
-    
+    useEffect(() => {
+        (async () => {
+            const albumData = await fetch('http://127.0.0.1:3000/entries').then(response => response.json());
+            setAlbums(albumData);
+        })();
+    },[]);
+
     return (
         <>
-            <Route exact path='/'>
-                <p>Check one two</p>
-            </Route>
-            <Route path='/:slug'>
-                <AlbumDetails />
-            </Route>
+            {!!albums.length ? (
+                <>
+                    <Route exact path='/'>
+                        <ul>
+                            {albums.map((album, index) => (
+                                <li key={index}>
+                                    <Link to={`/entries/${album.slug}`}>{album.album_name} - {album.band_name}</Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </Route>
+                    <Route path='/entries/:album_slug'>
+                        <AlbumDetails />
+                        <br/>
+                        <button type='button' onClick={() => history.goBack()}>Go Back</button>
+                    </Route>
+                </>
+            ) : (
+                <p>Loading CEOs...</p>
+            )}
         </>
-    )
-}
+    );
+};
 
 export default Albums;
